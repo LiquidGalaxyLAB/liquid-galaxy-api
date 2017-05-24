7ebfd69738f8a2f2b3ce20e051e724c5b85dec0a
zurfyx <zurfyx@gmail.com>
2017-05-25 00:02:13 +0200

    lint fix

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
-const config = require('config');
 
 const log = require('./helpers/log');
 const routes = require('./routes');
