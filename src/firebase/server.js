cb3ca4fe25c78d17b040ec151be233efe025cdb4
zurfyx <zurfyx@gmail.com>
2017-07-05 18:56:17 +0200

    firebase report ip

--- src/firebase/server.js
+++ src/firebase/server.js
@@ -1,4 +1,5 @@
 const firebase = require('firebase');
+const publicIp = require('public-ip');
 
 const { encodeUid } = require('./utils');
 
@@ -6,9 +7,17 @@ const SERVER_TIME = firebase.database.ServerValue.TIMESTAMP;
 
 async function reportAlive(uid) {
   const encodedUid = encodeUid(uid);
-  await firebase.database().ref(`/servers/${encodedUid}/lastOnline`).set(SERVER_TIME);
-  await firebase.database().ref(`/servers/${encodedUid}/isOnline`).set(true);
-  firebase.database().ref(`/servers/${encodedUid}/isOnline`).onDisconnect().set(false);
+  const encodedPublicIp = (await publicIp.v4()).replace(/\./g, ':');
+
+  const serverRef = firebase.database().ref(`/servers/${encodedUid}`);
+  await serverRef.child('lastOnline').set(SERVER_TIME);
+  await serverRef.child('isOnline').set(true);
+  serverRef.child('isOnline').onDisconnect().set(false);
+
+  const ipRef = firebase.database().ref(`/ips/${encodedPublicIp}/${encodedUid}`);
+  await ipRef.child('lastOnline').set(SERVER_TIME);
+  await ipRef.child('isOnline').set(true);
+  ipRef.child('isOnline').onDisconnect().set(false);
 }
 
 module.exports = {
