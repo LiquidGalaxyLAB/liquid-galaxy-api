e82b855f70e1ffca3ea85d615572601197ebfd3a
zurfyx <zurfyx@gmail.com>
2017-07-23 17:41:06 +0200

    duplicate displayName + hasPassword onto ips collection

--- src/firebase/server.js
+++ src/firebase/server.js
@@ -5,27 +5,38 @@ const { encodeUid } = require('./utils');
 
 const SERVER_TIME = firebase.database.ServerValue.TIMESTAMP;
 
-async function reportGeneralInfo({ uid, hasPassword, displayName }) {
+function serverRef(uid) {
   const encodedUid = encodeUid(uid);
-  const serverRef = firebase.database().ref(`/servers/${encodedUid}`);
+  return firebase.database().ref(`/servers/${encodedUid}`);
+}
 
-  await serverRef.child('hasPassword').set(hasPassword || false);
-  await serverRef.child('displayName').set(displayName || '');
+async function ipRef(uid) {
+  const encodedUid = encodeUid(uid);
+  const currentPublicIp = await publicIp.v4();
+  const encodedPublicIp = currentPublicIp.replace(/\./g, ':');
+  return firebase.database().ref(`/ips/${encodedPublicIp}/${encodedUid}`);
+}
+
+async function reportGeneralInfo({ uid, hasPassword = false, displayName = '' }) {
+  const server = serverRef(uid);
+  await server.child('hasPassword').set(hasPassword);
+  await server.child('displayName').set(displayName);
+
+  const ip = await ipRef(uid);
+  await ip.child('hasPassword').set(hasPassword);
+  await ip.child('displayName').set(displayName);
 }
 
 async function reportAlive(uid) {
-  const encodedUid = encodeUid(uid);
-  const encodedPublicIp = (await publicIp.v4()).replace(/\./g, ':');
+  const server = serverRef(uid);
+  await server.child('lastOnline').set(SERVER_TIME);
+  await server.child('isOnline').set(true);
+  server.child('isOnline').onDisconnect().set(false);
 
-  const serverRef = firebase.database().ref(`/servers/${encodedUid}`);
-  await serverRef.child('lastOnline').set(SERVER_TIME);
-  await serverRef.child('isOnline').set(true);
-  serverRef.child('isOnline').onDisconnect().set(false);
-
-  const ipRef = firebase.database().ref(`/ips/${encodedPublicIp}/${encodedUid}`);
-  await ipRef.child('lastOnline').set(SERVER_TIME);
-  await ipRef.child('isOnline').set(true);
-  ipRef.child('isOnline').onDisconnect().set(false);
+  const ip = await ipRef(uid);
+  await ip.child('lastOnline').set(SERVER_TIME);
+  await ip.child('isOnline').set(true);
+  ip.child('isOnline').onDisconnect().set(false);
 }
 
 module.exports = {
